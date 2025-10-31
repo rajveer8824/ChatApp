819e54b55051fbd92fd6736620d5c23fdc59e4a2
rajveer8824 <rjvrnth@gmail.com>
2025-10-31 23:20:41 +0530

    app completed finaly

--- backand/frontend/src/App.jsx
+++ backand/frontend/src/App.jsx
@@ -33,21 +33,26 @@ function App() {
             //   <Left />
             //   <Right />
             // </div>
-            <div className="drawer">
-  <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content">
-    {/* Page content here */}
-    <label htmlFor="my-drawer-1" className="btn drawer-button">Open drawer</label>
-  </div>
-  <div className="drawer-side">
-    <label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay"></label>
-    <ul className="menu bg-base-200 min-h-full w-80 p-4">
-      {/* Sidebar content here */}
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-    </ul>
-  </div>
-</div>
+         <div className="drawer lg:drawer-open">
+                <input
+                  id="my-drawer-2"
+                  type="checkbox"
+                  className="drawer-toggle"
+                />
+                <div className="drawer-content flex flex-col items-center justify-center">
+                  <Right />
+                </div>
+                <div className="drawer-side">
+                  <label
+                    htmlFor="my-drawer-2"
+                    aria-label="close sidebar"
+                    className="drawer-overlay"
+                  ></label>
+                  <ul className="menu w-80 min-h-full bg-black text-base-content">
+                    <Left />
+                  </ul>
+                </div>
+              </div>
           ) : (
             <Navigate to="/login" />
           )
